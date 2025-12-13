9e4aa9752165e02ed80ebd3fb0d3163a6ff31962
mohamed <mohamed@email.com>
2025-12-13 11:44:46 +0100

    added the trailing slash to login

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -27,7 +27,7 @@ const LoginPage = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         setLoading(true);
-        const response = await fetch(`https://tasktorch-todoapp.onrender.com/api/login`, {
+        const response = await fetch(`https://tasktorch-todoapp.onrender.com/api/login/`, {
             method: 'POST',
             headers:  {
                 'Content-Type': 'application/json'
